7bb9a7e489de941d1ac6581e8faac8c5cd1fa1b7
erfan <erfan.arefmehr@gmail.com>
2023-01-17 21:27:02 +0330

    properties

--- erfan-rfm/razm1/product.py
+++ erfan-rfm/razm1/product.py
@@ -9,28 +9,192 @@ class Product:
                  sale_price:float, manage_stock:bool, stock_quantity:int, date_created_gmt :int, date_modified_gmt:int,category_id:int = 0, 
                  is_visible = True, is_available:bool = False):
 
-        self.id = None
-        self.category_id = category_id
-        self.title = title
-        self.short_description =  short_description
-        self.description = description
-        self.slug = slug
-        self.permalink = permalink
-        self.is_available = is_available
-        self.sku = sku
-        self.price = price
-        self.regular_price = regular_price
-        self.sale_price = sale_price
-        self.manage_stock = manage_stock
-        self.stock_quantity = stock_quantity
-        self.is_visible = is_visible
+        self.__id = None
+        self.__category_id = category_id
+        self.__title = title
+        self.__short_description =  short_description
+        self.__description = description
+        self.__slug = slug
+        self.__permalink = permalink
+        self.__is_available = is_available
+        self.__sku = sku
+        self.__price = price
+        self.__regular_price = regular_price
+        self.__sale_price = sale_price
+        self.__manage_stock = manage_stock
+        self.__stock_quantity = stock_quantity
+        self.__is_visible = is_visible
         self.date_created_gmt = date_created_gmt
         self.date_modified_gmt = date_modified_gmt
-        self.db = None
+        self.__db = None
+        
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def category_id(self):
+        return self.__category_id
+    
+    @category_id.setter
+    def category_id(self, category_id: int):
+        if not isinstance(category_id, int):
+            raise ValueError('Category Id must be an integer')
+        elif category_id < 0:
+            raise ValueError('Category Id cannot be less than 0')
+        self.__category_id = category_id
+        
+    @property
+    def title(self):
+        return self.__title
+    
+    @title.setter
+    def title(self, title: str):
+        if not isinstance(title, str):
+            raise ValueError('Title must be a string')
+        self.__title = title
+        
+    @property
+    def short_description(self):
+        return self.__short_description
+    
+    @short_description.setter
+    def short_description(self, short_description: str):
+        if not isinstance(short_description, str):
+            raise ValueError('Short Description must be a string')
+        self.__short_description = short_description
+
+    @property
+    def description(self):
+        return self.__description
+    
+    @description.setter
+    def description(self, description: str):
+        if not isinstance(description, str):
+            raise ValueError('Short Description must be a string')
+        self.__description = description
+        
+    @property
+    def slug(self):
+        return self.__slug
+    
+    @slug.setter
+    def slug(self, slug: str):
+        if not isinstance(slug, str):
+            raise ValueError('Slug must be a string')
+        self.__slug = slug
+        
+    @property
+    def permalink(self):
+        return self.__permalink
+    
+    @permalink.setter
+    def permalink(self, permalink: str):
+        if not isinstance(permalink, str):
+            raise ValueError('Permalink must be a string')
+        self.__permalink = permalink
+        
+    @property
+    def is_available(self):
+        return self.__is_available
+
+    @is_available.setter
+    def is_available(self, is_available: bool):
+        if not isinstance(is_available, bool):
+            raise ValueError('Is Available must be a boolean')
+        self.__is_available = is_available
+        
+    @property
+    def sku(self):
+        return self.__sku
+    
+    @sku.setter
+    def sku(self, sku: str):
+        if not isinstance(sku, str):
+            raise ValueError('SKU must be a string')
+        self.__sku = sku
+        
+    @property
+    def price(self):
+        return self.__price
+    
+    @price.setter
+    def price(self, price: float):
+        if not isinstance(price, float):
+            raise ValueError('Price must be a float')
+        elif price < 0:
+            raise ValueError('Price cannot be less than 0')
+        self.__price = price
+        
+    @property
+    def regular_price(self):
+        return self.__regular_price
+    
+    @regular_price.setter
+    def regular_price(self, regular_price: float):
+        if not isinstance(regular_price, float):
+            raise ValueError('Regular Price must be a float')
+        elif regular_price < 0:
+            raise ValueError('Regular Price cannot be less than 0')
+        self.__regular_price = regular_price
+        
+    @property
+    def sale_price(self):
+        return self.__sale_price
+    
+    @sale_price.setter 
+    def sale_price(self, sale_price: float):
+        if not isinstance(sale_price, float):
+            raise ValueError('Sale Price must be a float')
+        elif sale_price < 0:
+            raise ValueError('Sale Price cannot be less than 0')
+        self.__sale_price = sale_price
+        
+    @property
+    def manage_stock(self):
+        return self.__manage_stock
+    
+    @manage_stock.setter
+    def manage_stock(self, manage_stock: bool):
+        if not isinstance(manage_stock, bool):
+            raise ValueError('Manage Stock must be a boolean')
+        self.__manage_stock = manage_stock
+        
+    @property
+    def stock_quantity(self):
+        return self.__stock_quantity
+    
+    @stock_quantity.setter
+    def stock_quantity(self, stock_quantity: int):
+        if not isinstance(stock_quantity, int):
+            raise ValueError('Stock Quantity must be an integer')
+        elif stock_quantity < 0:
+            raise ValueError('Stock Quantity cannot be less than 0')
+        self.__stock_quantity = stock_quantity
+        
+    @property
+    def is_visible(self):
+        return self.__is_visible
+    
+    @is_visible.setter
+    def is_visible(self, is_visible: bool):
+        if not isinstance(is_visible, bool):
+            raise ValueError('Is Visible must be a boolean')
+        self.__is_visible = is_visible
+        
+    @property
+    def db(self):
+        return self.__db
+    
+    @db.setter
+    def db(self, db_type: ProductInJsonDb | ProductInMemoryDb):
+        if not isinstance(db_type, (ProductInMemoryDb, ProductInJsonDb)):
+            raise ValueError('DB Type must be a ProductInMemoryDb or ProductInJsonDb instance')
+        self.__db = db_type
 
     def create(self, product_id:int, storage_type:str) -> str:
         self.db = StorageType(storage_type).get_db()
-        self.id = product_id
+        self.__id = product_id
         self.db.insert(
             {
                 "id" : self.id,
