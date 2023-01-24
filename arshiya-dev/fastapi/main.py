8d626e9c60eb4015798d9b118b7ac6c3db28f7ff
Arshiya Khalili <arshiya.pogrammer@gmail.com>
2023-01-24 20:20:41 +0330

    connect to database

--- arshiya-dev/fastapi/main.py
+++ arshiya-dev/fastapi/main.py
@@ -1,7 +1,13 @@
 from  fastapi import  FastAPI
+import mysql.connector
 
+
+mydb = mysql.connector.connect(host="localhost",user="arshiya",password="arshiya",database="python",port=3306)
+
+mycursor = mydb.cursor()
 
 app = FastAPI()
+
 movies = [
           {"title":"","year":0},
           {"title":"Batman","year":2021},
@@ -17,6 +23,9 @@ async def root():
 # get all movies
 @app.get("/movies")
 def get_movies():
+    sql = "SELECT * FROM movies"
+    mycursor.execute(sql)
+    movies = mycursor.fetchall() # function return everything by cursor
     return movies
 
 # get single movie
@@ -31,24 +40,20 @@ def delete_movie(movie_id:int):
     return  {"message":"movie has been deleted successfully"}
 
 # if you want to create or insert new things you have to use post request and post requesat never get parameter like delete
-@app.post("/movie")
+@app.post("/create_movie")
 def create_movie(movie:dict):
     movies.append(movie)
     return movies[-1]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+#update movie
+@app.post("/update_movie")
+def update_movie(movie_id:int,movie:dict):
+    movie_tobe_updated = movies[movie_id] # get movie to be updated
+    movie_tobe_updated['title'] = movie['title'] # update title
+    movie_tobe_updated['year'] = movie['year'] # update year
+    movies[movie_id] = movie_tobe_updated # has been updated successfully
+    return movie_tobe_updated
 
 
 
