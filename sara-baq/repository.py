bc91331d942ed696c5e9a24ccaf54c3df864493a
sarah bagheri <95619163+Sarahbaqeri@users.noreply.github.com>
2023-02-09 11:56:16 -0800

    modify file

--- sara-baq/repository.py
+++ sara-baq/repository.py
@@ -12,6 +12,7 @@ class BookRepo():
             _book.append(book)
         return _book
 
+
     @staticmethod
     async def insert(book : Book):
         id = str(uuid.uuid4())
@@ -23,19 +24,21 @@ class BookRepo():
         await database.get_collection('book').insert_one(_book)
 
 
-        @staticmethod
-        async def update(id:str,book : Book):
-            _book = await database.get_collection('book').find_one({"_id":id})
-            _book["title"] = book.title
-            _book["description"] = book.description
-            await database.get_collection("book").update_one({"_id":id,"$set":_book})
+    @staticmethod
+    async def update(id:str,book : Book):
+        _book = await database.get_collection('book').find_one({"_id":id})
+        _book["title"] = book.title
+        _book["description"] = book.description
+        await database.get_collection("book").update_one({"_id":id,"$set":_book})
 
-        @staticmethod
-        async def retrieve_id(id:str):
-            return await database.get_collection('book').find_one({"_id":id})
 
-        @staticmethod
-        async def delete(id:str):
-             await database.delete_collection('book').delete_one({"_id":id})
+    @staticmethod
+    async def retrieve_id(id:str):
+        return await database.get_collection('book').find_one({"_id":id})
 
-             
+
+    @staticmethod
+    async def delete(id:str):
+        await database.delete_collection('book').delete_one({"_id":id})
+
+
